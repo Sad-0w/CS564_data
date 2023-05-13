6beb8e3a2e15b9655d6ebc358331cb8bac656f88
Spencer Stevens <shs.shadowkitty@gmail.com>
2023-05-12 20:57:19 -0400

    removed shell script

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# encrypt
-# openssl rsautl -encrypt -inkey bob_public.pem -pubin -in top_secret.txt -out top_secret.enc
-
- git push https://dummy000000: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@github.com/Sad-0w/CS564_data.git
